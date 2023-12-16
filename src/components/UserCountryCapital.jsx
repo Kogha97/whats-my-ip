2a8d904e35569bba7da30ec36c549c12c3dced21
Kogha97 <poiuytrewq7991@gmail.com>
2023-12-16 12:02:35 +0000

    new api

--- src/components/UserCountryCapital.jsx
+++ src/components/UserCountryCapital.jsx
@@ -1,17 +1,18 @@
 import { useState, useEffect } from "react";
-
+const apiKey2 = 'e5294daba2a301c08275cc78c0a26e87'
 export default function UserCountryCapital({ code }) {
   const [capitalInfo, setCapitalInfo] = useState("");
 
   const fetchCountryData = async (code) => {
     const apiKey = import.meta.env.VITE_CAPITAL_API_KEY;
-    const url = `https://api.countrylayer.com/v2/alpha/${code}?access_key=${apiKey}`;
+    // const url = `https://api.countrylayer.com/v2/alpha/${code}?access_key=${apiKey}`;
+    const url = `http://api.ipstack.com/${code}?access_key=${apiKey2}`
 
     try {
       const response = await fetch(url);
       const data = await response.json();
-      setCapitalInfo(data.capital);
-      console.log(capitalInfo)
+      // setCapitalInfo(data.capital);
+      setCapitalInfo(data.location.capital)
     } catch (error) {
       console.error("Error fetching country captial data:", error);
     }

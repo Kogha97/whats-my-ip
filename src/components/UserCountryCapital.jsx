da4bc4fc0972d47818806a0258ffb316ec084a2a
Rebekka <rebekka@fake.com>
2023-12-16 12:49:19 +0100

    change api country layer to secure

--- src/components/UserCountryCapital.jsx
+++ src/components/UserCountryCapital.jsx
@@ -5,7 +5,7 @@ export default function UserCountryCapital({ code }) {
 
   const fetchCountryData = async (code) => {
     const apiKey = import.meta.env.VITE_CAPITAL_API_KEY;
-    const url = `http://api.countrylayer.com/v2/alpha/${code}?access_key=${apiKey}`;
+    const url = `https://api.countrylayer.com/v2/alpha/${code}?access_key=${apiKey}`;
 
     try {
       const response = await fetch(url);
